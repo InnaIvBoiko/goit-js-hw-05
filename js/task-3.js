d1bb3c2773c59e4f65ef02da77a13f551aec75a0
Inna Boiko <146464243+InnaIvBoiko@users.noreply.github.com>
2026-04-06 16:48:50 +0200

    Solve homework 5: async functions and array methods

--- js/task-3.js
+++ js/task-3.js
@@ -1,54 +1,43 @@
-const sortByDescendingFriendCount = (users) => users.toSorted((firstFriend, secondFfiend) =>
-    secondFfiend.friends.length - (firstFriend.friends.length));
-
-// const sortByDescendingFriendCount = (users) => {
-//     let friendsCount = 0;
-//     let newUsers = users;
-
-//   newUsers.forEach(user => {
-//     user.friendsCount = user.friends.length;
-//   });
-//   return newUsers.toSorted((firstFriendsCount, secondFfiendsCount) =>
-//     secondFfiendsCount.friendsCount - (firstFriendsCount.friendsCount));
-// };
+const sortByDescendingFriendCount = users =>
+  users.toSorted((firstFriend, secondFfiend) => secondFfiend.friends.length - firstFriend.friends.length);
 
 console.log(
   sortByDescendingFriendCount([
     {
-      name: "Moore Hensley",
-      friends: ["Sharron Pace"],
-      gender: "male"
+      name: 'Moore Hensley',
+      friends: ['Sharron Pace'],
+      gender: 'male',
     },
     {
-      name: "Sharlene Bush",
-      friends: ["Briana Decker", "Sharron Pace"],
-      gender: "female"
+      name: 'Sharlene Bush',
+      friends: ['Briana Decker', 'Sharron Pace'],
+      gender: 'female',
     },
     {
-      name: "Ross Vazquez",
-      friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-      gender: "male"
+      name: 'Ross Vazquez',
+      friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+      gender: 'male',
     },
     {
-      name: "Elma Head",
-      friends: ["Goldie Gentry", "Aisha Tran"],
-      gender: "female"
+      name: 'Elma Head',
+      friends: ['Goldie Gentry', 'Aisha Tran'],
+      gender: 'female',
     },
     {
-      name: "Carey Barr",
-      friends: ["Jordan Sampson", "Eddie Strong"],
-      gender: "male"
+      name: 'Carey Barr',
+      friends: ['Jordan Sampson', 'Eddie Strong'],
+      gender: 'male',
     },
     {
-      name: "Blackburn Dotson",
-      friends: ["Jacklyn Lucas", "Linda Chapman"],
-      gender: "male"
+      name: 'Blackburn Dotson',
+      friends: ['Jacklyn Lucas', 'Linda Chapman'],
+      gender: 'male',
     },
     {
-      name: "Sheree Anthony",
-      friends: ["Goldie Gentry", "Briana Decker"],
-      gender: "female"
-    }
+      name: 'Sheree Anthony',
+      friends: ['Goldie Gentry', 'Briana Decker'],
+      gender: 'female',
+    },
   ])
 );
 // [
